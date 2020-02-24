e101d07855513c5b9db618c62eaf09a30105c444
李海涛 <liht@dustess.com>
2020-02-25 00:16:39 +0800

    feat: 完成内容和标题复制到剪贴板

--- packages/daily-helper/src/App.vue
+++ packages/daily-helper/src/App.vue
@@ -18,11 +18,7 @@ export default class App extends Vue {}
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  max-width: 750px;
 }
 </style>
